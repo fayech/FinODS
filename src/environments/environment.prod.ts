fc8a3030c1f256681949e7f512f4377c453cf64d
fayech <mehdifayech@gmail.com>
2021-06-30 10:36:08 +0100

    my changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase : {
+  apiKey: "AIzaSyCrdDJR8juzQZYavyRaLctWduj7ByH7pgo",
+  authDomain: "eaze-d5bc4.firebaseapp.com",
+  databaseURL: "https://eaze-d5bc4.firebaseio.com",
+  projectId: "eaze-d5bc4",
+  storageBucket: "eaze-d5bc4.appspot.com",
+  messagingSenderId: "870984862497",
+  appId: "1:870984862497:web:2999da7e2e9de7f113cc60",
+  measurementId: "G-3VB35F1N83"
+  }
 };
